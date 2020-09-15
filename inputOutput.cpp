cc78a93c76f8bbd62c141e435e1377a08f18e6fe
Saena <toastyhatesthesea@gmail.com>
2020-09-14 19:11:56 -0700

    Finished

--- inputOutput.cpp
+++ inputOutput.cpp
@@ -1,10 +1,22 @@
 #include <string>
 #include <stdio.h>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
 
+using namespace std;
 
 int main()
 {
-    printf("Hello");
 
+    int a, b, c;
 
+    cin >> a >> b >> c;
+    int total = a + b + c;
+
+    cout << total;
+
+    return 0;
 }
